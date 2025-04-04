0a9883525d1da13b4a61eda9330dff88bbb4e53d
Dongwoo Shin <socra167@gmail.com>
2025-04-04 11:37:20 +0900

    Update HomeController.kt

--- src/main/kotlin/com/example/back/domain/home/home/controller/HomeController.kt
+++ src/main/kotlin/com/example/back/domain/home/home/controller/HomeController.kt
@@ -1,13 +1,17 @@
 package com.example.back.domain.home.home.controller
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HomeController {
 
+    @Value("\${custom.secretValue}")
+    private lateinit var secretValue: String
+    
     @GetMapping
     fun home(): String {
-        return "Hello, World123"
+        return "Hello, World, $secretValue"
     }
 }
