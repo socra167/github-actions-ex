2d9ed2983e0639012cc52e4418975f7c03c47b90
socra167 <socra167@gmail.com>
2025-04-04 11:19:31 +0900

    update example kotlin

---
+++ src/main/kotlin/com/example/back/domain/home/home/controller/HomeController.kt
@@ -0,0 +1,13 @@
+package com.example.back.domain.home.home.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HomeController {
+
+    @GetMapping
+    fun home(): String {
+        return "Hello, World123"
+    }
+}
